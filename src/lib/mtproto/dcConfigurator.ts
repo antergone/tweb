6c6d5d9354de208d7e83dfce605af7f651d5806a
Simon Ma <chnmayu@gmail.com>
2024-06-23 05:28:24 +0800

    change host

--- src/lib/mtproto/dcConfigurator.ts
+++ src/lib/mtproto/dcConfigurator.ts
@@ -47,8 +47,8 @@ export function constructTelegramWebSocketUrl(dcId: DcId, connectionType: Connec
 
   const suffix = getTelegramConnectionSuffix(connectionType);
   const path = connectionType !== 'client' ? 'apiws' + TEST_SUFFIX + (premium ? PREMIUM_SUFFIX : '') : ('apiws' + TEST_SUFFIX);
-  const chosenServer = `wss://${App.suffix.toLowerCase()}ws${dcId}${suffix}.web.telegram.org/${path}`;
-  // const chosenServer = `wss://ws.tg.simonmy.com/${App.suffix.toLowerCase()}ws${dcId}${suffix}/${path}`;
+  // const chosenServer = `wss://${App.suffix.toLowerCase()}ws${dcId}${suffix}.web.telegram.org/${path}`;
+  const chosenServer = `wss://ws.tg.simonmy.com/${App.suffix.toLowerCase()}ws${dcId}${suffix}/${path}`;
   return chosenServer;
 }
 
@@ -101,8 +101,8 @@ export class DcConfigurator {
       const suffix = getTelegramConnectionSuffix(connectionType);
       const subdomain = this.sslSubdomains[dcId - 1] + suffix;
       const path = Modes.test ? 'apiw_test1' : 'apiw1';
-      chosenServer = 'https://' + subdomain + '.web.telegram.org/' + path;
-      // chosenServer = 'https://api.tg.simonmy.com/'+subdomain + '/' + path;
+      // chosenServer = 'https://' + subdomain + '.web.telegram.org/' + path;
+      chosenServer = 'https://api.tg.simonmy.com/'+subdomain + '/' + path;
     } else {
       for(const dcOption of this.dcOptions) {
         if(dcOption.id === dcId) {
